da849016c762162bdafab5e9ee5e3e2b4427c5ed
WsWSC <wsc19970805@gmail.com>
2025-03-12 12:01:43 +0800

    modified pc_reg

--- rtl/if_id.v
+++ rtl/if_id.v
@@ -3,3 +3,16 @@
 *                                                *
 * create by WsWSC                                *
 **************************************************
+
+module if_id(
+    input wire clk,
+    input wire rst,
+    input wire[31:0] inst_i,
+    input wire[31:0] inst_addr_i,
+
+    output wire[31:0] inst_addr_o,
+    output wire[31:0] inst_o
+);
+
+
+endmodule
---
+++ rtl/ifetch.v
@@ -0,0 +1,22 @@
+**************************************************
+* risc v side project                            *
+*                                                *
+* create by WsWSC                                *
+**************************************************
+
+module pc_reg(
+    //from pc
+    input wire[31:0] pc_addr_i,
+    //from rom
+    input wire[31:0] rom_inst_i, 
+    
+    //to rom 
+    output wire[31:0] if2rom_addr_o,
+    //to if_id
+    output wire[31:0] inst_addr_o,
+    output wire[31:0] inst_o
+);
+
+
+
+endmodule
