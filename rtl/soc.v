d042f1fac85a379abc0402e0c4643a936971d1c9
WsWSC <wsc19970805@gmail.com>
2026-01-21 15:52:37 +0800

    phase 1 done, test with tb.v & simulation

--- rtl/soc.v
+++ rtl/soc.v
@@ -6,9 +6,37 @@
 ////////////////////////////////////////////////////////////
 
 module soc(
-
+    input   wire        clk,
+    input   wire        rst_n
 );
 
+    // ============================================================
+    //  Wire Declarations
+    // ============================================================
 
+    // core to rom
+    wire[31:0]  core_inst_addr_o;
+
+    // rom to core
+    wire[31:0]  rom_inst_o;
+
+
+    // ============================================================
+    //  Module Instantiation & Interconnection
+    // ============================================================
+
+    core core_inst(
+        .clk            (clk),
+        .rst_n          (rst_n),
+        .inst_i         (rom_inst_o),
+        
+        .inst_addr_o    (core_inst_addr_o)
+    );
+
+    rom rom_inst(
+        .inst_addr_i    (core_inst_addr_o),
+
+        .inst_o         (rom_inst_o)
+    );
 
 endmodule
